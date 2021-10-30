d2cc5f02a14370789a356922b64805db0de79435
Kcs111 <kcs111@txstate.edu>
2021-10-30 12:13:44 -0500

    It begins Script

---
+++ script.js
@@ -0,0 +1,50 @@
+// start the game
+
+var startGameBtnEl = document.querySelector("#start-game-btn");
+
+var startScreenEl = document.querySelector("#start-screen");
+
+// Quiz content
+
+var quizScreenEl = document.querySelector("#quiz-content");
+
+var headerEl = document.querySelector("header");
+
+var timeLeftEl = document.querySelector("#time-left");
+
+// Highscore Element
+
+var viewHighscoresAEl = document.querySelector("#view-highscores");
+
+//finished Screen
+
+
+var postGameScreenEl = document.querySelector("#post-game-screen");
+
+
+//user-score
+var userScoreEl = document.querySelector("#user-score");
+
+
+//start over button
+
+var playAgainBtnEl = document.querySelector("#play-again-btn");
+
+
+//add-highscore button
+var addHighscoreBtnEl = document.querySelector("#add-highscore-btn");
+
+
+var highScoresListEl = document.querySelector("#high-scores-list");
+
+var answerInfoEl = document.querySelector("#answer-info");
+
+var answerInfoTextEl = document.querySelector("#answer-info-text");
+
+
+//highscore stuff
+var gameEndFormEl = document.querySelector("#game-end-form");
+
+var userInitials = document.querySelector("#user-initials");
+
+var clearHighScoresBtnEl = document.querySelector("#clear-highscore");
